495de5f3a1e29ce3d1abc00daa8b5c3f3eaa7b42
zhy <123456>
2019-10-20 00:21:37 +0800

    重要更新

--- js/index.js
+++ js/index.js
@@ -20,10 +20,10 @@ var RoleProto = {
 	direction: 'halt',
 	status: 'ok',
 	step: 2, // 精灵每次移动的步长值
-	tileLeft: 0, // 精灵所在的格子
-	tileTop: 0,
 	tileWidth: 1, // 精灵所占几个格子
 	tileHeight: 1,
+  tileLeft: 0, // 精灵所在的格子
+  tileTop: 0,
   life: 1,
   hp: 100,
   mp: 100,
@@ -34,10 +34,10 @@ var RoleProto = {
   level: 1,
   name: '',
   camp: 'player',
-  id: 0,
+  id: 1,
 };
 var Roles = []; // 角色们
-var Id = 0; // 主索引, 唯一标识
+var Id = 1; // 主索引, 唯一标识
 
 // 执行入口
 window.onload = function () {
@@ -45,20 +45,14 @@ window.onload = function () {
 		WinSize = cc.director.getWinSize();
     cc.view.adjustViewPort(true);
     cc.view.resizeWithBrowserSize(true);
-    cc.view.setDesignResolutionSize(
-      WinSize.width,
-      WinSize.height,
-      cc.ResolutionPolicy.SHOW_ALL
-    );
+    cc.view.setDesignResolutionSize(WinSize.width, WinSize.height, cc.ResolutionPolicy.SHOW_ALL);
     cc.audioEngine.setMusicVolume(0.2);
     cc.audioEngine.setEffectsVolume(0.2);
 
 		cc.loader.loadJson('resources.json', function(_, data) {
 			Res = data;
 			var resArr = [];
-			for (var i in Res) {
-				resArr.push(Res[i]);
-			}
+			for (var i in Res) resArr.push(Res[i]);
 
 			cc.LoaderScene.preload(resArr, function () {
 				var scene = cc.Scene.extend({
@@ -77,13 +71,24 @@ window.onload = function () {
 						Context.addChild(LayerBullet);
 						Context.addChild(LayerGoods);
 
-						// 加载并绘制场景(地块)
 						cc.loader.loadJson(Res.scene0, function(_, data) {
               MapTile = data[1];
-							funcDrawScene(data[0]);
-						});
+							funcDrawScene(data[0]); // 绘制场景(地块)
 
-						funcRoleAdd('player'); // 创建玩家控制的精灵
+							// 初始化角色, 子弹, 物品所在层的地图数组
+              for (var i = 0; i < MapTile.length; i += 1) {
+                MapRole.push([]);
+                MapBullet.push([]);
+                MapGoods.push([]);
+                for (var j = 0; j < MapTile[i].length; j += 1) {
+                  MapRole[i].push(0);
+                  MapBullet[i].push(0);
+                  MapGoods[i].push(0);
+                }
+              }
+
+              funcRoleAdd('player'); // 创建玩家控制的精灵
+						});
 
 						// 处理按键
 						var mapping = {
@@ -94,36 +99,31 @@ window.onload = function () {
 							onKeyPressed: function(keyCode) {
 								if (mapping[keyCode]) {
 									// 按下的是方向键
-                  for (let i = 0; i < Roles.length; i += 1) {
-                    const role = Roles[i];
+                  for (var i = 0; i < Roles.length; i += 1) {
+                    var role = Roles[i];
                     if (role.camp !== 'player') continue;
-
-                    if (role.status === 'ok') {
-                      role.direction = mapping[keyCode];
-                      if (!role.action) {
-                        role.action = role.sprite.runAction(
-                          cc.RepeatForever.create(cc.Sequence.create(role.animate))
-                        );
-                      }
-                    }
+                    if (role.status !== 'ok') break;
+                    role.direction = mapping[keyCode];
+                    if (role.action) break;
+                    role.action = role.sprite.runAction(
+                      cc.RepeatForever.create(cc.Sequence.create(role.animate))
+                    );
                     break;
                   }
 								}
 							},
 							onKeyReleased: function() {
-							  for (let i = 0; i < Roles.length; i += 1) {
-							    const role = Roles[i];
+							  for (var i = 0; i < Roles.length; i += 1) {
+							    var role = Roles[i];
 							    if (role.camp !== 'player') continue;
-
-							    if (role.status === 'ok') {
-							      role.direction = '';
-							      if (role.action) {
-							        role.sprite.stopAction(role.action);
-							        role.action = 0;
-                    }
-                    role.sprite.texture = Res.kodFighterOther;
-							      role.sprite.setTextureRect(cc.rect(0, 0, 45, 68));
-                  }
+							    if (role.status !== 'ok') break;
+                  role.direction = 'halt';
+                  role.sprite.texture = Res.kodFighterOther;
+                  role.sprite.setTextureRect(cc.rect(0, 0, 45, 68));
+                  if (!role.action) break;
+                  role.sprite.stopAction(role.action);
+                  role.action = 0;
+                  break;
                 }
 							},
 						}, Context);
@@ -132,7 +132,7 @@ window.onload = function () {
 						this._super();
 						cc.eventManager.removeListener(cc.EventListener.KEYBOARD);
 					},
-					// update: funcRun,
+					update: funcRun,
 				});
 				cc.director.runScene(new scene());
 			}, this);
@@ -141,35 +141,71 @@ window.onload = function () {
   cc.game.run("gameCanvas");
 };
 
-// 实时处理
+// 实时运行
 function funcRun() {
-	if (!Sprite) return;
-	switch (Direction) {
-		case 0:
-		case undefined:
-			// 首先忽略掉无效按键
-			break;
-		case 1:
-			// 上
-			Sprite.y += Step;
-			break;
-		case 5:
-			// 下
-			Sprite.y -= Step;
-			break;
-		case 2:
-			// 左
-			Sprite.x -= Step;
-			Sprite.flippedX = true;
-			break;
-		case 4:
-			// 右
-			Sprite.x += Step;
-			Sprite.flippedX = false;
-			break;
-		default:
-			break;
-	}
+  for (var i = 0; i < Roles.length; i += 1) {
+    var role = Roles[i];
+    if (role.status !== 'ok' || role.direction === 'halt') continue;
+
+    var step = role.step;
+
+    // 碰撞检测
+    var proboX = role.sprite.x;
+    var probeY = role.sprite.y;
+
+    switch (role.direction) {
+      case 'up':
+        probeY += step;
+        break;
+      case 'down':
+        probeY -= step;
+        break;
+      case 'left':
+        if (!role.sprite.flippedX) role.sprite.flippedX = true;
+        else proboX -= step;
+        break;
+      case 'right':
+        if (role.sprite.flippedX) role.sprite.flippedX = false;
+        else proboX += step;
+        break;
+      default:
+        break;
+    }
+
+    // 检测角色是否越界
+    if (
+      proboX < 0 || proboX + role.tileWidth * TileSize > MapTile[0].length * TileSize ||
+      probeY < 0 || probeY + role.tileHeight * TileSize > MapTile.length * TileSize
+    ) continue;
+
+    // 检测角色是否碰到了墙(砖)
+    var left = proboX / TileSize;
+    var top = probeY / TileSize;
+    left = left.toFixed() - 0;
+    top = top.toFixed() - 0;
+    cc.log(MapTile[top][left], MapTile[top][left + role.tileWidth], MapTile[top + role.tileHeight][left], MapTile[top + role.tileHeight][left + role.tileWidth]);
+    if (
+      MapTile[top][left] ||
+      MapTile[top][left + role.tileWidth] ||
+      MapTile[top + role.tileHeight][left] ||
+      MapTile[top + role.tileHeight][left + role.tileWidth]
+    ) continue;
+
+    role.sprite.x = proboX;
+    role.sprite.y = probeY;
+
+    // 更新角色在地图上所处的位置
+    left = role.sprite.x / TileSize;
+    top = role.sprite.y / TileSize;
+    role.tileLeft = left.toFixed() - 0;
+    role.tileTop = top.toFixed() - 0;
+
+    for (var i = 0; i < role.tileHeight; i += 1) {
+      for (var j = 0; j < role.tileWidth; j += 1) {
+        MapRole[role.tileTop + i][role.tileLeft + j] = role.id;
+      }
+    }
+  }
 }
 
 // 绘制场景
@@ -204,13 +240,29 @@ function funcRoleAdd(roleType) {
 
 	switch (roleType) {
 		case 'player':
+		  var width = 45 / TileSize;
+		  var height = 68 / TileSize;
+		  role.tileWidth = width.toFixed() - 0;
+		  role.tileHeight = height.toFixed() - 0;
+		  // role.tileLeft = parseInt(MapTile[0].length / 2);
+		  // role.tileTop = parseInt(MapTile.length / 2);
+
+		  // 在角色层地图数组上标记角色的标识(id)
+		  for (var i = 0; i < role.tileHeight; i += 1) {
+		    for (var j = 0; j < role.tileWidth; j += 1) {
+		      MapRole[role.tileTop + i][role.tileLeft + j] = role.id;
+        }
+      }
+
       // 精灵的站立状态
       var imgPath = Res.kodFighterOther;
       var sprite = cc.Sprite.create(imgPath, cc.rect(0, 0, 45, 68));
       sprite.attr({
-        x: WinSize.width / 2,
-        y: WinSize.height / 2,
         tag: Id,
+        anchorX: 0,
+        anchorY: 0,
+        x: role.tileLeft * TileSize,
+        y: role.tileTop * TileSize,
       });
 
       // 精灵的行走状态
