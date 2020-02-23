eefe5464b5cfe7bee5ba9d1e3ea603d764ad1155
zhy2018 <15234579277@139.com>
2020-02-24 02:31:24 +0800

    制作欢迎页。

--- js/3.js
+++ js/3.js
@@ -65,7 +65,7 @@ window.onload = function() {
 		}
 
 		cc.LoaderScene.preload(resData, function() {
-			var sceneMain = cc.Scene.extend({
+			var sceneWelcome = cc.Scene.extend({
 				onEnter: function() {
 					this._super();
 					var winSize = cc.director.getWinSize();
@@ -73,6 +73,131 @@ window.onload = function() {
 					h = winSize.height;
 					control.winWidth = w;
 					control.winHeight = h;
+					var bgSize = 256;
+					var scaleX = w / bgSize;
+					scaleX = scaleX.toFixed(3) - 0;
+					var scaleY = h / bgSize;
+					scaleY = scaleY.toFixed(3) - 0;
+
+					// 底层
+					var layer = cc.Layer.create();
+					layer.attr({
+						y: h,
+						anchorX: 0,
+						anchorY: 0,
+						scaleX: scaleX,
+						scaleY: scaleY,
+					});
+					this.addChild(layer);
+
+					// 背景图
+					var bg = cc.Sprite.create(res.bg, cc.rect(0, 0, bgSize, 130));
+					bg.attr({
+						anchorX: 0,
+						anchorY: 1,
+					});
+					layer.addChild(bg);
+
+					// 云朵, 青山, 浮云 [y, height]
+					var datas = [
+						[0, 30], [63, 32], [96, 21], [119, 7],
+						[130, 80], [210, 16], [226, 14], [240, 16],
+					];
+					var times = [2, 3, 4, 8, 10, 5, 3, 2];
+					for (var i = 0; i < datas.length; i += 1) {
+						var data = datas[i];
+						var time = times[i];
+						var sprite = cc.Sprite.create(
+							res.bg,
+							cc.rect(i < 4 ? bg.width : 0, data[0], bg.width, data[1]),
+						);
+						sprite.attr({
+							y: -data[0],
+							anchorX: 0,
+							anchorY: 1,
+						});
+						layer.addChild(sprite);
+						sprite.runAction(cc.repeatForever(cc.sequence(
+							cc.moveTo(time, cc.p(-bg.width, -data[0])),
+							cc.moveTo(0, cc.p(i < 4 ? bg.width : 0, -data[0])),
+						)));
+
+						if (i >= 4) {
+							var sprite2 = cc.Sprite.create(
+								res.bg,
+								cc.rect(0, data[0], bg.width, data[1]),
+							);
+							sprite2.attr({
+								x: bg.width,
+								y: -data[0],
+								anchorX: 0,
+								anchorY: 1,
+							});
+							layer.addChild(sprite2);
+							sprite2.runAction(cc.repeatForever(cc.sequence(
+								cc.moveTo(time, cc.p(0, -data[0])),
+								cc.moveTo(0, cc.p(bg.width, -data[0])),
+							)));
+						}
+					}
+
+					// logo
+					var logo = cc.Sprite.create(res.img, cc.rect(0, 120, 189, 96));
+					logo.attr({
+						x: w / 2,
+						y: h / 3 * 2,
+						scale: scaleX,
+					});
+					this.addChild(logo);
+
+					var btn = cc.LabelTTF.create('开始游戏', '黑体', 24 * scaleX);
+					btn.attr({
+						x: w / 2,
+						y: h / 4,
+						scaleX: 0.75,
+						lineWidth: 2 * scaleX,
+						strokeStyle: cc.color(0, 0, 0, 255),
+					});
+					this.addChild(btn);
+					btn.runAction(cc.RepeatForever.create(cc.Blink.create(1, 1)));
+
+					var mask = cc.LayerColor.create(funcColor('#000000'), w, h);
+					mask.attr({ opacity: 0 });
+					this.addChild(mask);
+
+					cc.eventManager.addListener({
+						event: cc.EventListener.TOUCH_ONE_BY_ONE,
+						onTouchBegan: function(touch, e) {
+							var target = e.getCurrentTarget();
+							if (target != btn) return false;
+
+							var loc = target.convertToNodeSpace(touch.getLocation());
+							var size = target.getContentSize();
+							var rect = cc.rect(0, 0, size.width, size.height);
+							if (!cc.rectContainsPoint(rect, loc)) return false;
+
+							btn.stopAllActions();
+							btn.runAction(cc.RepeatForever.create(cc.Blink.create(1, 5)));
+							btn.scheduleOnce(function() {
+								mask.runAction(cc.FadeIn.create(0.5));
+								btn.scheduleOnce(function() {
+									cc.director.runScene(new sceneMain());
+								}, 0.5);
+							}, 0.5);
+						}
+					}, btn);
+				},
+				onExit: function() {
+					this._super();
+					cc.eventManager.removeListener(cc.EventListener.TOUCH_ONE_BY_ONE);
+				},
+			});
+
+			var sceneMain = cc.Scene.extend({
+				onEnter: function() {
+					this._super();
+					w = control.winWidth;
+					h = control.winHeight;
 
 					// 计算格子的缩放倍数
 					control.zoom = w / config.mapSizeLimit / config.tileSize;
@@ -86,10 +211,13 @@ window.onload = function() {
 					control.layerScene = cc.LayerColor.create(funcColor(color), w, h);
 					this.addChild(control.layerScene);
 
+					var mask = cc.LayerColor.create(funcColor('#ffffff'), w, h);
+					this.addChild(mask);
+
 					// 背景图
 					var scale = w / 264;
 					scale = scale.toFixed(3) - 0;
-					var bg = cc.Sprite.create(res.bg);
+					var bg = cc.Sprite.create(res.bg, cc.rect(0, 256, 264, 208));
 					bg.attr({
 						y: h,
 						anchorX: 0,
@@ -119,10 +247,7 @@ window.onload = function() {
 					layerStage.addChild(layerStageRole);
 
 					// 格子的选择框
-					var tileBorder = cc.Sprite.create(
-						res.img,
-						cc.rect(32, 32, 30, 30)
-					);
+					var tileBorder = cc.Sprite.create(res.img, cc.rect(32, 32, 30, 30));
 					tileBorder.attr({
 						scale: control.zoom,
 						visible: false
@@ -156,6 +281,7 @@ window.onload = function() {
 					cc.loader.loadJson(res.stage, function(_, data) {
 						control.stageData = data;
 						funcInit();
+						mask.runAction(cc.FadeOut.create(0.5));
 					});
 
 					funcShowHeroInfo();
@@ -166,7 +292,8 @@ window.onload = function() {
 					cc.eventManager.removeListener(cc.EventListener.TOUCH_ONE_BY_ONE);
 				},
 			});
-			cc.director.runScene(new sceneMain());
+
+			cc.director.runScene(new sceneWelcome());
 		}, this);
 	};
 	cc.game.run("gameCanvas");
